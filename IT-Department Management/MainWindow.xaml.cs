5382dc455b497f24ca42baa347e777467304ad0b
ZiganshinRS <ZiganshinRS@gmail.com>
2020-01-19 15:20:36 +0300

    Добавьте файлы проекта.

---
+++ IT-Department Management/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using IT_Department_Management.View;
+
+namespace IT_Department_Management
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCurrentTasks_Click(object sender, RoutedEventArgs e)
+        {
+            //RecipientList recipientList = new RecipientList();
+            //recipientList.Owner = this;
+            //recipientList.ShowDialog();
+        }
+
+        private void btnComputers_Click(object sender, RoutedEventArgs e)
+        {
+            ComputersList computersList = new ComputersList();
+            computersList.Owner = this;
+            computersList.ShowDialog();
+        }
+    }
+}
